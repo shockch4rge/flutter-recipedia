b808c769699299c4c3201f2d6af056e7fe19297a
Shockch4rge <favouriteteo@gmail.com>
2022-05-23 09:42:57 +0800

    Moved UserHeadline inline into PostHeader

--- lib/widgets/post/post_content.dart
+++ lib/widgets/post/post_content.dart
@@ -1,15 +1,15 @@
-import 'dart:ui';
-
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_recipedia/main.dart';
+import 'package:flutter_recipedia/widgets/common/avatar.dart';
+import 'package:flutter_recipedia/widgets/post/post_buttons.dart';
 import 'package:flutter_recipedia/widgets/post/post_options.dart';
 
 import '../../models/recipe.dart';
+import '../../screens/view_recipe_screen.dart';
 
-class UserHeadline extends StatelessWidget {
-  const UserHeadline({Key? key}) : super(key: key);
+class PostHeader extends StatelessWidget {
+  final Recipe recipe;
+  const PostHeader({Key? key, required this.recipe}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,17 +20,8 @@ class UserHeadline extends StatelessWidget {
           style: TextButton.styleFrom(padding: EdgeInsets.zero),
           icon: CachedNetworkImage(
             imageUrl: "flutter-recipedia.appspot.com/lisapfp2.png",
-            imageBuilder: (context, imageProvider) => Container(
-              width: 36,
-              margin: const EdgeInsets.only(right: 8),
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                  image: imageProvider,
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
+            imageBuilder: (context, imageProvider) =>
+                Avatar(size: 36, avatarUrl: recipe.imageUrl),
             placeholder: (context, url) => Container(
               width: 20,
               height: 20,
@@ -43,7 +34,7 @@ class UserHeadline extends StatelessWidget {
             ),
             errorWidget: (context, url, error) => const Icon(Icons.error),
           ),
-          label: Text("lisahannigan",
+          label: Text(recipe.author.username,
               style: Theme.of(context).textTheme.headline4),
         )
       ],
@@ -51,33 +42,9 @@ class UserHeadline extends StatelessWidget {
   }
 }
 
-class UserHeadlineMock extends StatelessWidget {
-  const UserHeadlineMock({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton.icon(
-      onPressed: () {},
-      style: TextButton.styleFrom(
-          padding: EdgeInsets.zero, splashFactory: NoSplash.splashFactory),
-      icon: Container(
-        width: 36,
-        margin: const EdgeInsets.only(right: 8),
-        decoration: const BoxDecoration(
-          shape: BoxShape.circle,
-          image: DecorationImage(
-            image: AssetImage("assets/images/post_placeholder.jpg"),
-            fit: BoxFit.cover,
-          ),
-        ),
-      ),
-      label: Text("lisahannigan", style: Theme.of(context).textTheme.headline4),
-    );
-  }
-}
-
-class PostHeader extends StatelessWidget {
-  const PostHeader({Key? key}) : super(key: key);
+class PostHeaderMock extends StatelessWidget {
+  final Recipe recipe;
+  const PostHeaderMock({Key? key, required this.recipe}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -86,9 +53,20 @@ class PostHeader extends StatelessWidget {
       padding: const EdgeInsets.symmetric(horizontal: 15),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: const [
-          UserHeadlineMock(),
-          PostOptions(),
+        children: [
+          TextButton.icon(
+            onPressed: () {},
+            style: TextButton.styleFrom(
+                padding: EdgeInsets.zero,
+                splashFactory: NoSplash.splashFactory),
+            icon: Avatar(
+              size: 36,
+              avatarUrl: recipe.imageUrl,
+            ),
+            label: Text(recipe.author.username,
+                style: Theme.of(context).textTheme.headline4),
+          ),
+          const PostOptions(),
         ],
       ),
     );
@@ -96,15 +74,16 @@ class PostHeader extends StatelessWidget {
 }
 
 class PostImageMock extends StatelessWidget {
-  const PostImageMock({Key? key}) : super(key: key);
+  final String imageUrl;
+  const PostImageMock({Key? key, required this.imageUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 400,
-      decoration: const BoxDecoration(
+      decoration: BoxDecoration(
         image: DecorationImage(
-          image: AssetImage("assets/images/post_placeholder.jpg"),
+          image: AssetImage(imageUrl),
           fit: BoxFit.cover,
         ),
       ),
@@ -150,8 +129,8 @@ class PostContent extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const PostHeader(),
-        const PostImageMock(),
+        PostHeaderMock(recipe: recipe),
+        PostImageMock(imageUrl: recipe.imageUrl),
         Container(
           padding: const EdgeInsets.only(left: 22, right: 22, bottom: 30),
           child: Column(
@@ -163,116 +142,51 @@ class PostContent extends StatelessWidget {
                   children: [
                     Row(
                       children: [
-                        SizedBox(
-                          width: 55,
-                          child: ElevatedButton.icon(
-                            style: ButtonStyle(
-                              alignment: Alignment.centerLeft,
-                              elevation: MaterialStateProperty.all(0),
-                              backgroundColor:
-                                  MaterialStateProperty.all(Colors.transparent),
-                              padding:
-                                  MaterialStateProperty.all(EdgeInsets.zero),
-                            ),
-                            onPressed: () {},
-                            icon: App.heartOutlinedIcon,
-                            label: const Text(
-                              "14",
-                              style: TextStyle(color: App.primaryAccent),
-                            ),
-                          ),
+                        LikeButton(
+                          onPressed: () {},
                         ),
-                        SizedBox(
-                          width: 50,
-                          child: ElevatedButton.icon(
-                            style: ButtonStyle(
-                                alignment: Alignment.centerLeft,
-                                elevation: MaterialStateProperty.all(0),
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.transparent),
-                                padding:
-                                    MaterialStateProperty.all(EdgeInsets.zero)),
-                            onPressed: () {},
-                            icon: App.chatBubbleIcon,
-                            label: const Text(
-                              "3",
-                              style: TextStyle(color: App.primaryAccent),
-                            ),
-                          ),
+                        CommentButton(
+                          onPressed: () {},
                         ),
-                        SizedBox(
-                          width: 20,
-                          child: ElevatedButton(
-                            style: ButtonStyle(
-                                alignment: Alignment.centerLeft,
-                                elevation: MaterialStateProperty.all(0),
-                                backgroundColor: MaterialStateProperty.all(
-                                    Colors.transparent),
-                                padding:
-                                    MaterialStateProperty.all(EdgeInsets.zero)),
-                            onPressed: () {},
-                            child: App.shareIcon,
-                          ),
-                        ),
+                        ShareButton(
+                          onPressed: () {},
+                        )
                       ],
                     ),
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: ElevatedButton(
-                        onPressed: () =>
-                            Navigator.of(context).pushNamed("/view-recipe"),
-                        style: ButtonStyle(
-                          padding: MaterialStateProperty.all(EdgeInsets.zero),
-                          shape: MaterialStateProperty.all(
-                              RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(8))),
-                        ),
-                        child: Ink(
-                          decoration: BoxDecoration(
-                            gradient: const LinearGradient(
-                                colors: [App.primaryAccent, App.primaryColor]),
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Container(
-                            padding: const EdgeInsets.symmetric(horizontal: 14),
-                            constraints: const BoxConstraints(
-                              minWidth: 88.0,
-                              minHeight: 36.0,
-                            ), // min sizes for Material buttons
-                            alignment: Alignment.center,
-                            child: const Text(
-                              "View Recipe",
-                              textAlign: TextAlign.center,
-                              style: TextStyle(fontSize: 12),
-                            ),
-                          ),
-                        ),
+                    ViewRecipeButton(
+                      onPressed: () => Navigator.of(context).pushNamed(
+                        ViewRecipeScreen.routeName,
+                        arguments: recipe,
                       ),
                     ),
                   ],
                 ),
               ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Truffle Spaghetti Aglio e Olio Carbonara",
-                    style: Theme.of(context).textTheme.headline3,
-                  ),
-                  const SizedBox(height: 14),
-                  Text(
-                    "lisahannigan:",
-                    style: Theme.of(context).textTheme.headline4,
-                  ),
-                  const SizedBox(height: 8),
-                  Text(
-                    "I was planning to make a beef stew for tonight, but I got lazy midway and decided to whip up this quick carbonara instead! Quick and easy recipe (with no dumb cream)!",
-                    style: Theme.of(context).textTheme.bodyText2,
-                  ),
-                ],
-              ),
+              _buildDescription(context),
             ],
           ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildDescription(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          recipe.title,
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        const SizedBox(height: 14),
+        Text(
+          "${recipe.author.username}:",
+          style: Theme.of(context).textTheme.headline4,
+        ),
+        const SizedBox(height: 8),
+        Text(
+          recipe.description,
+          style: Theme.of(context).textTheme.bodyText2,
         ),
       ],
     );
