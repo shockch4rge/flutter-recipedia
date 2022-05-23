62fb3c4aa445caf6c61efeaa9e11aeb73dd1678c
Shockch4rge <favouriteteo@gmail.com>
2022-05-23 14:33:05 +0800

    Refactored layout code

--- lib/screens/view_recipe_screen.dart
+++ lib/screens/view_recipe_screen.dart
@@ -1,7 +1,15 @@
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_recipedia/models/recipe.dart';
+import 'package:flutter_recipedia/utils/get_args.dart';
+import 'package:flutter_recipedia/widgets/common/avatar.dart';
+import 'package:flutter_recipedia/widgets/common/checkbox_list_item.dart';
+import 'package:flutter_recipedia/widgets/post/post_buttons.dart';
 import 'package:flutter_recipedia/widgets/view_recipe/snapping_app_bar.dart';
 
 class ViewRecipeScreen extends StatefulWidget {
+  static const String routeName = "/view-recipe";
+
   const ViewRecipeScreen({Key? key}) : super(key: key);
 
   @override
@@ -13,6 +21,7 @@ class _ViewRecipeScreenState extends State<ViewRecipeScreen> {
 
   double get minHeight => kToolbarHeight + MediaQuery.of(context).padding.top;
   double get maxHeight => 200 + MediaQuery.of(context).padding.top;
+  Recipe get recipe => getArgs<Recipe>(context);
 
   @override
   Widget build(BuildContext context) {
@@ -26,27 +35,16 @@ class _ViewRecipeScreenState extends State<ViewRecipeScreen> {
           physics: const BouncingScrollPhysics(),
           controller: _controller,
           slivers: [
-            SnappingAppBar(maxHeight: maxHeight, minHeight: minHeight),
+            SnappingAppBar(
+                recipe: recipe, maxHeight: maxHeight, minHeight: minHeight),
             SliverPadding(
-              padding: const EdgeInsets.all(16),
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
               sliver: SliverToBoxAdapter(
                 child: Column(
                   children: [
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
-                    _buildCard(1),
+                    _buildRecipeInteractions(),
+                    _buildRecipeDescription(),
+                    _buildRecipeDirections(),
                   ],
                 ),
               ),
@@ -54,6 +52,99 @@ class _ViewRecipeScreenState extends State<ViewRecipeScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  _buildRecipeInteractions() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          children: [
+            Avatar(
+              size: 40,
+              avatarUrl: recipe.author.avatarUrl,
+            ),
+            const SizedBox(width: 5),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  recipe.author.name,
+                  style: Theme.of(context).textTheme.subtitle1,
+                ),
+                const SizedBox(height: 3),
+                Text(
+                  "@${recipe.author.username}",
+                  style: Theme.of(context).textTheme.headline5,
+                )
+              ],
+            )
+          ],
+        ),
+        Row(
+          children: [
+            LikeButton(onPressed: () {}),
+            CommentButton(onPressed: () {}),
+            ShareButton(onPressed: () {}),
+          ],
+        )
+      ],
+    );
+  }
+
+  _buildRecipeDescription() {
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 24),
+      child: Text(
+        recipe.description,
+        style: Theme.of(context).textTheme.bodyText2,
+      ),
+    );
+  }
+
+  _buildRecipeDirections() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "Ingredients",
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        Column(
+          children: recipe.ingredients
+              .map((ingredient) => CheckBoxListItem(title: ingredient))
+              .toList(),
+        ),
+        const SizedBox(height: 20),
+        Text(
+          "Directions",
+          style: Theme.of(context).textTheme.headline3,
+        ),
+        Column(
+          children: recipe.steps
+              .mapIndexed(
+                (index, step) => Container(
+                  margin: const EdgeInsets.symmetric(vertical: 12),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Step ${index + 1}:",
+                        style: Theme.of(context).textTheme.headline4,
+                      ),
+                      const SizedBox(height: 5),
+                      Text(
+                        step,
+                        style: Theme.of(context).textTheme.bodyText1,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+              .toList(),
+        )
+      ],
     );
   }
 
