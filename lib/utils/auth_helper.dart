ec9425da11bc59034406c2d3aa439d84fca3811c
Shockch4rge <favouriteteo@gmail.com>
2022-05-26 14:53:26 +0800

    Slight refactor for AuthHelper

--- lib/utils/auth_helper.dart
+++ lib/utils/auth_helper.dart
@@ -1,15 +1,14 @@
 class AuthHelper {
-  static get upperCasePattern => RegExp('((.*[A-Z]){2})');
-  static get lowerCasePattern => RegExp('((.*[a-z]){3})');
-  static get specialCharsPattern => RegExp('((.*[!@#\$&*]){1})');
-  static get digitsPattern => RegExp('((.*[0-9]){2})');
-  static get lengthPattern => RegExp('.{8}');
-
+  static final upperCasePattern = RegExp('((.*[A-Z]){2})');
+  static final lowerCasePattern = RegExp('((.*[a-z]){3})');
+  static final specialCharsPattern = RegExp('((.*[!@#\$&*]){1})');
+  static final digitsPattern = RegExp('((.*[0-9]){2})');
+  static final lengthPattern = RegExp('.{8}');
   static final emailPattern = RegExp(
       '/^(([^<>()[]\\.,;:s@"]+(.[^<>()[]\\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))');
 
   static isEmail(String email) {
-    return emailPattern.allMatches(email).isNotEmpty;
+    return match(email, emailPattern);
   }
 
   static isPassword(String password) {
