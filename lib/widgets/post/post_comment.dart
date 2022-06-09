d43abeaca53bb643a9e07989a83e1107e1d62c87
Shockch4rge <favouriteteo@gmail.com>
2022-06-09 19:47:55 +0800

    Fixed overflowed widget width on different screens

--- lib/widgets/post/post_comment.dart
+++ lib/widgets/post/post_comment.dart
@@ -41,78 +41,149 @@ class PostComment extends StatelessWidget {
           )
         ],
       ),
+      // child: Container(
+      //   padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      //   child: Row(
+      //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //     children: [
+      //       Row(
+      //         crossAxisAlignment: CrossAxisAlignment.start,
+      //         children: [
+      //           const Padding(
+      //             padding: EdgeInsets.only(top: 4, right: 6),
+      //             child: Avatar(
+      //               size: 40,
+      //               avatarUrl: "assets/images/avatar_placeholder.png",
+      //             ),
+      //           ),
+      //           Expanded(
+      //             child: Column(
+      //               crossAxisAlignment: CrossAxisAlignment.start,
+      //               children: [
+      //                 Text(
+      //                   "johndoe123",
+      //                   style: TextStyle(
+      //                     fontSize: 14,
+      //                     fontWeight: FontWeight.w500,
+      //                     color: Theme.of(context).primaryColorDark,
+      //                   ),
+      //                 ),
+      //                 const SizedBox(height: 6),
+      //                 const Text(
+      //                   "I love this recipe! Highly recommend it. dwdwdwdwdwdwdwdwdwdw",
+      //                   style: TextStyle(
+      //                     fontSize: 13,
+      //                     color: Colors.black,
+      //                   ),
+      //                 ),
+      //                 const SizedBox(height: 8),
+      //                 Row(
+      //                   children: [
+      //                     GestureDetector(
+      //                       onTap: () {},
+      //                       child: const Text(
+      //                         "1.2k likes",
+      //                         style:
+      //                             TextStyle(fontSize: 12, color: Colors.grey),
+      //                       ),
+      //                     ),
+      //                     const SizedBox(width: 12),
+      //                     GestureDetector(
+      //                       // TODO: amend mock user
+      //                       onTap: () => onReply(mockUser),
+      //                       child: const Text(
+      //                         "Reply",
+      //                         style: TextStyle(
+      //                             fontSize: 12,
+      //                             fontWeight: FontWeight.w500,
+      //                             color: Colors.grey),
+      //                       ),
+      //                     ),
+      //                   ],
+      //                 )
+      //               ],
+      //             ),
+      //           )
+      //         ],
+      //       ),
+      //       IconButton(
+      //         onPressed: () {},
+      //         splashRadius: 18,
+      //         icon: const Icon(
+      //           FeatherIcons.heart,
+      //           size: 18,
+      //         ),
+      //       )
+      //     ],
+      //   ),
+      // ),
       child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+        padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 10),
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const Padding(
-                  padding: EdgeInsets.only(top: 4, right: 6),
-                  child: Avatar(
-                    size: 40,
-                    avatarUrl: "assets/images/avatar_placeholder.png",
-                  ),
-                ),
-                SizedBox(
-                  width: 260,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "johndoe123",
-                        style: TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                          color: Theme.of(context).primaryColorDark,
-                        ),
-                      ),
-                      const SizedBox(height: 6),
-                      const Text(
-                        "I love this recipe! Highly recommend it. dwdwdwdwdwdwdwdwdwdw",
-                        style: TextStyle(
-                          fontSize: 13,
-                          color: Colors.black,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          GestureDetector(
-                            onTap: () {},
-                            child: const Text(
-                              "1.2k likes",
-                              style:
-                                  TextStyle(fontSize: 12, color: Colors.grey),
-                            ),
-                          ),
-                          const SizedBox(width: 12),
-                          GestureDetector(
-                            // TODO: amend mock user
-                            onTap: () => onReply(mockUser),
-                            child: const Text(
-                              "Reply",
-                              style: TextStyle(
-                                  fontSize: 12,
-                                  fontWeight: FontWeight.w500,
-                                  color: Colors.grey),
-                            ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
-              ],
+            const Expanded(
+              child: Avatar(
+                size: 40,
+                avatarUrl: "assets/images/avatar_placeholder.png",
+              ),
             ),
-            IconButton(
-              onPressed: () {},
-              splashRadius: 18,
-              icon: const Icon(
-                FeatherIcons.heart,
-                size: 18,
+            Expanded(
+              flex: 4,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "johndoe123",
+                    style: TextStyle(
+                      fontSize: 14,
+                      fontWeight: FontWeight.w500,
+                      color: Theme.of(context).primaryColorDark,
+                    ),
+                  ),
+                  const SizedBox(height: 6),
+                  const Text(
+                    "I love this recipe! Highly recommend it. Will make it again sometime!",
+                    style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.black,
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {},
+                        child: const Text(
+                          "1.2k likes",
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ),
+                      const SizedBox(width: 12),
+                      GestureDetector(
+                        // TODO: amend mock user
+                        onTap: () => onReply(mockUser),
+                        child: const Text(
+                          "Reply",
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w500,
+                              color: Colors.grey),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              child: IconButton(
+                onPressed: () {},
+                splashRadius: 18,
+                icon: const Icon(
+                  FeatherIcons.heart,
+                  size: 18,
+                ),
               ),
             )
           ],
