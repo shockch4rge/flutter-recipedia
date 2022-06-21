2808a15cb5cca7932bff3f8c708150db2098730e
Shockch4rge <favouriteteo@gmail.com>
2022-06-21 14:40:17 +0800

    Reworked auth process

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -4,30 +4,30 @@ import 'package:flutter_recipedia/models/user.dart' as local;
 
 class AuthProvider with ChangeNotifier {
   final auth.FirebaseAuth _firebaseAuth;
-  late local.User user;
+  late final local.User user;
 
   AuthProvider(this._firebaseAuth);
 
   Stream<auth.User?> get authStateChanges => _firebaseAuth.authStateChanges();
 
-  Future<String> signIn(
-      {required String email, required String password}) async {
-    try {
-      await _firebaseAuth.signInWithEmailAndPassword(
-        email: email,
-        password: password,
-      );
-      return "signed in";
-    } on auth.FirebaseAuthException catch (e) {
-      return e.message!;
-    }
+  Future<auth.UserCredential> signIn({
+    required String email,
+    required String password,
+  }) async {
+    final credentials = await _firebaseAuth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    return credentials;
   }
 
   Future<String> signUp(
       {required String email, required String password}) async {
     try {
       await _firebaseAuth.createUserWithEmailAndPassword(
-          email: email, password: password);
+        email: email,
+        password: password,
+      );
       return "signed up";
     } on auth.FirebaseAuthException catch (e) {
       return e.message!;
