f43d1c3e77099a9ab360ad86f1105f5baabc5d95
Shockch4rge <favouriteteo@gmail.com>
2022-06-25 01:20:42 +0800

    No longer need Firestore converters when referring to collections

--- lib/repositories/recipe_repository.dart
+++ lib/repositories/recipe_repository.dart
@@ -1,21 +1,37 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_recipedia/models/recipe.dart';
+import 'package:flutter_recipedia/models/user.dart';
 
 class RecipeRepository {
-  final CollectionReference _recipes;
+  final CollectionReference<Recipe> _recipes;
 
   const RecipeRepository(this._recipes);
 
   Future<List<Recipe>> getUserRecipes(DocumentReference userId) async {
-    final snap = await _recipes
-        .withConverter<Recipe>(
-          fromFirestore: Recipe.fromFirestore,
-          toFirestore: Recipe.toFirestore,
-        )
-        .where(Recipe.authorIdField, isEqualTo: userId)
-        .get();
+    final snap =
+        await _recipes.where(Recipe.authorIdField, isEqualTo: userId).get();
 
     final recipes = snap.docs.map((doc) => doc.data()).toList();
+    return recipes;
+  }
+
+  Future<List<Recipe>> getFollowedRecipes(DocumentReference userId) async {
+    final user = await userId.get();
+    final authorIds =
+        await user.get(User.followingField) as List<DocumentReference<User>>;
+
+    final List<Recipe> recipes = [];
+
+    for (final authorId in authorIds) {
+      final snap = await _recipes
+          .where(
+            Recipe.authorIdField,
+            isEqualTo: authorId,
+          )
+          .get();
+      recipes.addAll(snap.docs.map((doc) => doc.data()));
+    }
+
     return recipes;
   }
 }
--- lib/repositories/user_repository.dart
+++ lib/repositories/user_repository.dart
@@ -2,17 +2,12 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_recipedia/models/user.dart';
 
 class UserRepository {
-  final CollectionReference _users;
+  final CollectionReference<User> _users;
 
   const UserRepository(this._users);
 
-  Future<User> getUserById(DocumentReference userId) async {
-    final snap = await userId
-        .withConverter<User>(
-          fromFirestore: User.fromFirestore,
-          toFirestore: User.toFirestore,
-        )
-        .get();
+  Future<User> getUserById(DocumentReference<User> userId) async {
+    final snap = await userId.get();
 
     return snap.data()!;
   }
@@ -21,28 +16,81 @@ class UserRepository {
     required String name,
     required String username,
   }) async {
-    final userRef = await _users.add({
-      "name": name,
-      "username": username,
-      "bio": "",
-      "avatarUrl":
-          "https://firebasestorage.googleapis.com/v0/b/flutter-recipedia.appspot.com/o/recipedia_avatar_placeholder.png?alt=media&token=ab372ee6-b439-4566-a421-396b1e7735a2",
-    });
+    final userRef = _users.doc();
+
+    await userRef.set(
+      User(
+        id: userRef,
+        name: name,
+        username: username,
+        bio: "",
+        avatarUrl:
+            "https://firebasestorage.googleapis.com/v0/b/flutter-recipedia.appspot.com/o/recipedia_avatar_placeholder.png?alt=media&token=ab372ee6-b439-4566-a421-396b1e7735a2",
+        following: [],
+        followers: [],
+      ),
+    );
 
     return User.fromFirestore(await userRef.get(), null);
   }
 
-  Future<List<User>> getFollowers(DocumentReference userId) async {
-    final snap = await userId.get();
-    List<DocumentReference> followers = snap.get("followers");
+  Future<List<User>> getUserFollowers(DocumentReference userId) async {
+    final snap = await _users
+        .withConverter(
+          fromFirestore: User.fromFirestore,
+          toFirestore: User.toFirestore,
+        )
+        .where(User.followingField, arrayContains: userId)
+        .get();
 
-    return Future.wait(followers.map((f) async {
-      final snap = await f
-          .withConverter(
-              fromFirestore: User.fromFirestore, toFirestore: User.toFirestore)
-          .get();
+    return snap.docs.map((doc) => doc.data()).toList();
+  }
 
-      return snap.data()!;
-    }));
+  Future<List<User>> getUserFollowing(DocumentReference userId) async {
+    final snap = await _users
+        .withConverter(
+          fromFirestore: User.fromFirestore,
+          toFirestore: User.toFirestore,
+        )
+        .where(User.followersField, arrayContains: userId)
+        .get();
+
+    return snap.docs.map((doc) => doc.data()).toList();
+  }
+
+  Future<void> addUserFollower(
+    DocumentReference userId,
+    DocumentReference followerId,
+  ) async {
+    await userId.update({
+      User.followersField.components[0]: FieldValue.arrayUnion([followerId]),
+    });
+  }
+
+  Future<void> removeUserFollower(
+    DocumentReference userId,
+    DocumentReference followerId,
+  ) async {
+    await userId.update({
+      User.followersField.components[0]: FieldValue.arrayRemove([followerId]),
+    });
+  }
+
+  Future<void> addUserFollowing(
+    DocumentReference userId,
+    DocumentReference followingId,
+  ) async {
+    await userId.update({
+      User.followingField.components[0]: FieldValue.arrayUnion([followingId]),
+    });
+  }
+
+  Future<void> removeUserFollowing(
+    DocumentReference userId,
+    DocumentReference followingId,
+  ) async {
+    await userId.update({
+      User.followingField.components[0]: FieldValue.arrayRemove([followingId]),
+    });
   }
 }
