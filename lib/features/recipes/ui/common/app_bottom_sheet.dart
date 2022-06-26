0a08edc58e0894e4c97be210e1985bbcdc862076
Shockch4rge <favouriteteo@gmail.com>
2022-06-27 02:53:05 +0800

    Default app bottom sheet for future usage

---
+++ lib/features/recipes/ui/common/app_bottom_sheet.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class AppBottomSheet extends StatelessWidget {
+  final List<AppBottomSheetAction> actions;
+  const AppBottomSheet({Key? key, required this.actions}) : super(key: key);
+
+  static const defaultShape = RoundedRectangleBorder(
+    borderRadius: BorderRadius.only(
+      topLeft: Radius.circular(8),
+      topRight: Radius.circular(8),
+    ),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(top: 6),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            height: 5,
+            width: 40,
+            margin: const EdgeInsets.only(bottom: 3),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(100),
+              color: Colors.grey.shade400.withOpacity(0.7),
+            ),
+          ),
+          Column(
+            children: [
+              for (final action in actions) ...[
+                ListTile(
+                  onTap: action.onPressed,
+                  title: Wrap(
+                    spacing: 24,
+                    crossAxisAlignment: WrapCrossAlignment.center,
+                    children: [
+                      action.icon,
+                      Text(
+                        action.title,
+                        style: const TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Divider(
+                  height: 0,
+                  thickness: 1,
+                ),
+              ]
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class AppBottomSheetAction {
+  final Icon icon;
+  final String title;
+  final VoidCallback onPressed;
+
+  const AppBottomSheetAction({
+    required this.onPressed,
+    required this.icon,
+    required this.title,
+  });
+}
