698d8606fcf4c31d475568474ce9042f3e208181
Shockch4rge <favouriteteo@gmail.com>
2022-07-28 23:55:48 +0800

    Added text in case no recipes are liked

--- lib/features/users/ui/view_liked_recipes/view_liked_recipes_screen.dart
+++ lib/features/users/ui/view_liked_recipes/view_liked_recipes_screen.dart
@@ -2,15 +2,22 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_recipedia/features/users/ui/view_liked_recipes/widgets/view_liked_recipes_app_bar.dart';
 import 'package:flutter_recipedia/models/recipe.dart';
+import 'package:flutter_recipedia/providers/auth_provider.dart';
 import 'package:flutter_recipedia/repositories/recipe_repository.dart';
 import 'package:flutter_recipedia/utils/extensions/async_helper.dart';
-import 'package:flutter_recipedia/utils/mock_data.dart';
 import 'package:provider/provider.dart';
 
-class ViewLikedRecipesScreen extends StatelessWidget {
+class ViewLikedRecipesScreen extends StatefulWidget {
   static const routeName = "/home/profile/me/liked";
 
   const ViewLikedRecipesScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ViewLikedRecipesScreen> createState() => _ViewLikedRecipesScreenState();
+}
+
+class _ViewLikedRecipesScreenState extends State<ViewLikedRecipesScreen> {
+  late final userId = context.read<AuthProvider>().user!.id;
 
   @override
   Widget build(BuildContext context) {
@@ -18,12 +25,19 @@ class ViewLikedRecipesScreen extends StatelessWidget {
       appBar: const ViewLikedRecipesAppBar(),
       body: FutureBuilder(
         future:
-            context.read<RecipeRepository>().getLikedRecipes(userId: mockMeId),
+            context.read<RecipeRepository>().getLikedRecipes(userId: userId),
         builder: (context, snap) {
           if (snap.waiting) {
             return Container();
           }
+
           final likedRecipes = snap.data as List<Recipe>;
+
+          if (likedRecipes.isEmpty) {
+            return const Center(
+              child: Text("You haven't liked any recipes yet."),
+            );
+          }
 
           return Padding(
             padding: const EdgeInsets.all(24),
